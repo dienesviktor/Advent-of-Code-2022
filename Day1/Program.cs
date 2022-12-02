e78e178d18593092ce6a72dbe248238ac92da8b0
Viktor Dienes <dienes.viktor@nexogen.hu>
2022-12-02 19:18:45 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿string input = "input.txt";
+
+string[] data = File.ReadAllLines(input);
+
+List<int> all = new();
+
+int temp = 0;
+
+foreach (string line in data)
+{
+    if (line != string.Empty)
+    {
+        temp += Int32.Parse(line);
+    }
+    else
+    {
+        all.Add(temp);
+        temp = 0;
+    }
+}
+
+int mostCalories = all.Max();
+int sumOfTopThree = all.OrderByDescending(x => x).Take(3).Sum();
+
+Console.WriteLine($"Total calories carried by the best performer elf: {mostCalories}");
+Console.WriteLine($"Sum of calories carried by the top three elves: {sumOfTopThree}");
