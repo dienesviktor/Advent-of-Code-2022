379def56dfd6986e20ce13af540440d4d46cfa5f
Viktor Dienes <dienes.viktor@nexogen.hu>
2022-12-03 19:11:35 +0100

    Day 3: Part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -11,7 +11,7 @@ int PartOne()
         string firstCompartment = rucksack.Substring(0, rucksack.Length / 2);
         string secondCompartment = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
 
-        var commonItem = firstCompartment.Intersect(secondCompartment).First();
+        char commonItem = firstCompartment.Intersect(secondCompartment).First();
 
         int priority = char.IsLower(commonItem) ? (int)(commonItem - '0') - 48 : (int)(commonItem - '0') + 10;
 
@@ -21,4 +21,40 @@ int PartOne()
     return itemsPriorities.Sum();
 }
 
+int PartTwo()
+{
+    List<int> groupPriorities = new();
+
+    List<List<string>> groups = new();
+
+    for(int rucksack = 0; rucksack < data.Length; rucksack+=3)
+    {
+        string first = data[rucksack];
+        string second = data[rucksack + 1];
+        string third = data[rucksack + 2];
+
+        groups.AddRange(new List<List<string>> { new List<string> { first, second, third } });
+    }
+
+    foreach (var group in groups)
+    {
+        char commonItem = '\0';
+
+        foreach (char item in group.First())
+        {
+            if (group[1].Contains(item) && group[2].Contains(item))
+            {
+                commonItem = item;
+            }
+        }
+
+        int priority = char.IsLower(commonItem) ? (int)(commonItem - '0') - 48 : (int)(commonItem - '0') + 10;
+
+        groupPriorities.Add(priority);
+    }
+
+    return groupPriorities.Sum();
+}
+
 Console.WriteLine($"Sum of priorities of common items: {PartOne()}");
+Console.WriteLine($"Sum of priorities of groups' common items: {PartTwo()}");
