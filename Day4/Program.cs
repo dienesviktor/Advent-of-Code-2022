aa180953e1f3cbbaea02ea8af8d023e1d6e526b8
dienesviktor <dienesviktor@gmail.com>
2022-12-04 10:23:27 +0100

    Day 4: Part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -20,4 +20,23 @@ int PartOne()
     return counter;
 }
 
+int PartTwo()
+{
+    int counter = 0;
+
+    foreach(string line in data)
+    {
+        (int, int) firstElfRange = (int.Parse(line.Split(",")[0].Split("-")[0]), int.Parse(line.Split(",")[0].Split("-")[1]));
+        (int, int) secondElfRange = (int.Parse(line.Split(",")[1].Split("-")[0]), int.Parse(line.Split(",")[1].Split("-")[1]));
+
+        if (firstElfRange.Item1 >= secondElfRange.Item1 && firstElfRange.Item1 <= secondElfRange.Item2 || firstElfRange.Item2 >= secondElfRange.Item1 && firstElfRange.Item2 <= secondElfRange.Item2 || secondElfRange.Item1 >= firstElfRange.Item1 && secondElfRange.Item1 <= firstElfRange.Item2 || secondElfRange.Item2 >= firstElfRange.Item1 && secondElfRange.Item2 <= firstElfRange.Item2)
+        {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
 Console.WriteLine($"Number of assignment pairs that one range fully contains the other: {PartOne()}");
+Console.WriteLine($"Number of assignment pairs that ranges overlap: {PartTwo()}");
