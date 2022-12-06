b3cdaf3d4913b10c67373eac245c1e5592e3ca5a
Viktor Dienes <dienes.viktor@nexogen.hu>
2022-12-06 22:57:30 +0100

    Day 6: Part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+string input = "input.txt";
+
+char[] data = File.ReadAllLines(input)[0].ToCharArray();
+
+bool IsUniqueCharacters(string str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = i + 1; j < str.Length; j++)
+        {
+            if (str[i] == str[j]) return false;
+        }
+    }
+
+    return true;
+}
+
+int PartOne()
+{
+    for (int i = 0; i < data.Length; i++)
+    {
+        StringBuilder temp = new StringBuilder();
+
+        if (i > 3)
+        {
+            for (int j = i - 4; j < i; j++)
+            {
+                temp.Append(data[j]);
+            }
+
+            if (IsUniqueCharacters(temp.ToString()))
+            {
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+Console.WriteLine($"Number of characters needs to be processed before the first start-of-packet marker is detected: {PartOne()}");
